b3f6394e0893c30d0f59c2e5f246637161c2506b
sasikala subramani <sasikala050995@gmail.com>
2023-01-18 22:36:20 +0530

    resolved conflict

--- src/main/java/com/qa/opencart/pages/CheckoutPage.java
+++ src/main/java/com/qa/opencart/pages/CheckoutPage.java
@@ -8,14 +8,12 @@ public class CheckoutPage {
 		String name ="macbook";
 		
 	}
-	
-<<<<<<< HEAD
 	public void test() {
 		System.out.println("test method");
-=======
+	}
+
 	public void run() {
 		System.out.println("run method");
->>>>>>> ce3b45585426ec0d9bbaefe6bbebd4d31b3f3a3f
 	}
 
 }
