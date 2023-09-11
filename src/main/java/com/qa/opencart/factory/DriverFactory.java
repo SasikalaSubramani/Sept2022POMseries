3fd1f7ac7f4efbc838c722ec0d8a04d428f065ff
sasikala subramani <sasikala050995@gmail.com>
2023-09-11 22:41:19 +0530

    updated selenoid code

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -8,6 +8,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -20,82 +21,73 @@ import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverFactory {
 
+	public WebDriver driver;
 	public Properties prop;
 	public OptionsManager optionsManager;
 
 	public static String highlight;
 
 	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
+	
+	public static final Logger log = Logger.getLogger(DriverFactory.class);
+
 
 	public WebDriver initDriver(Properties prop) {
 
-		String browserName = prop.getProperty("browser").trim();//from properties file
-//		String browserName = System.getProperty("browser");// CMD LINE argument
-		System.out.println("Launching browser : " + browserName);
+		String browserName = prop.getProperty("browser").trim();
+		// String browserName = System.getProperty("browser");//CMD line argument
+		System.out.println("Browser name is : " + browserName);
+		log.info("Browser name is : " + browserName);
 
 		highlight = prop.getProperty("highlight");
 
 		optionsManager = new OptionsManager(prop);
 
-		switch (browserName.toLowerCase()) {
-		case "chrome":
-
+		if (browserName.equalsIgnoreCase("chrome")) {
+			log.info("running tests on chrome browser....");
 			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
-				System.out.println(" inside remote condition");
+				log.info("running tests on remote machine....");
 				// remote execution on docker/grid/cloud
-
 				init_remoteDriver("chrome");
-			}
-
-			else {
+			} else {
 				// local execution
-				System.out.println("inside else part of remote condition");
+				log.info("running tests locally....");
 				tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
-
 			}
-			break;
 
-		case "firefox":
-//			driver = new FirefoxDriver(optionsManager.getFirefoxOptions());
+		} else if (browserName.equalsIgnoreCase("firefox")) {
 
 			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
 				// remote execution on docker/grid/cloud
-
 				init_remoteDriver("firefox");
-			}
-
-			else {
+			} else {
 				// local execution
 				tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
-
 			}
 
-			break;
+		}
 
-		case "edge":
-//			driver = new EdgeDriver();
+		else if (browserName.equalsIgnoreCase("edge")) {
+
 			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
 				// remote execution on docker/grid/cloud
-
 				init_remoteDriver("edge");
-			}
-
-			else {
+			} else {
 				// local execution
 				tlDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
-
 			}
 
-			break;
+		}
 
-		case "safari":
-//			driver = new SafariDriver();
+		else if (browserName.equalsIgnoreCase("safari")) {
 			tlDriver.set(new SafariDriver());
-			break;
+		}
 
-		default:
-			System.out.println("plz pass the right browser...." + browserName);
-			break;
+		else {
+			log.error("please pass the right browser...."+ browserName);
+			System.out.println("Please pass the right browser name...." + browserName);
+			
+			//info, warn, error, fatal
 		}
 
 		getDriver().manage().deleteAllCookies();
@@ -103,6 +95,7 @@ public class DriverFactory {
 		getDriver().get(prop.getProperty("url"));
 
 		return getDriver();
+
 	}
 
 	private void init_remoteDriver(String browserName) {
@@ -147,38 +140,47 @@ public class DriverFactory {
 		FileInputStream ip = null;
 
 		// mvn clean install -Denv="qa"
+		// mvn clean install
 
 		String envName = System.getProperty("env");
-		System.out.println("--------->Running test cases on environment------>" + envName);
+		System.out.println("-----> Running test cases on environment----> " + envName);
 
 		if (envName == null) {
-			System.out.println("No env is given......hence running the test cases on QA env......");
+			System.out.println("No env is given...hence running it on default QA env....");
 			try {
 				ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
-		} else
-			try {
+		} else {
 
+			try {
 				switch (envName.toLowerCase()) {
 				case "qa":
 					ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
 					break;
+				case "dev":
+					ip = new FileInputStream("./src/test/resources/config/dev.config.properties");
+					break;
 				case "stage":
 					ip = new FileInputStream("./src/test/resources/config/stage.config.properties");
+					break;
+				case "uat":
+					ip = new FileInputStream("./src/test/resources/config/uat.config.properties");
 					break;
 				case "prod":
 					ip = new FileInputStream("./src/test/resources/config/config.properties");
 					break;
 
 				default:
-					System.out.println("Please pass the right env name........." + envName);
+					System.out.println("Please pass the right env name...." + envName);
 					break;
 				}
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
+
+		}
 
 		try {
 			prop.load(ip);
@@ -187,7 +189,6 @@ public class DriverFactory {
 		}
 
 		return prop;
-
 	}
 
 	/**
--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -7,71 +7,75 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class OptionsManager {
-	
+
 	private Properties prop;
 	private ChromeOptions co;
 	private FirefoxOptions fo;
 	private EdgeOptions eo;
-	
+
 	public OptionsManager(Properties prop) {
 		this.prop = prop;
 	}
-	
+
 	public ChromeOptions getChromeOptions() {
 		co = new ChromeOptions();
 		
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			co.setCapability("enableVNC", true);
-			co.setPlatformName("linux");	
+			co.setPlatformName("linux");
+			co.setBrowserVersion(prop.getProperty("browserversion"));
 		}
-		if(Boolean.parseBoolean(prop.getProperty("headless"))) {
-			System.out.println("......Running the tests in headless mode");
+		
+		
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println(".....Running the test in Headless mode.......");
 			co.setHeadless(true);
 		}
-		if(Boolean.parseBoolean(prop.getProperty("incognito"))) {
-			System.out.println("......Running the tests in incognito mode");
+		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
+			System.out.println(".....Running the test in Incognito mode.......");
 			co.addArguments("--incognito");
 		}
 		return co;
-		
 	}
+
 	public FirefoxOptions getFirefoxOptions() {
 		fo = new FirefoxOptions();
 		
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			fo.setCapability("enableVNC", true);
-			fo.setPlatformName("linux");	
+			fo.setPlatformName("linux");
+			fo.setBrowserVersion(prop.getProperty("browserversion"));
 		}
 		
-		if(Boolean.parseBoolean(prop.getProperty("headless"))) {
-			System.out.println("......Running the tests in headless mode");
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println(".....Running the test in Headless mode.......");
 			fo.setHeadless(true);
 		}
-		if(Boolean.parseBoolean(prop.getProperty("incognito"))) {
-			System.out.println("......Running the tests in incognito mode");
+		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
+			System.out.println(".....Running the test in Incognito mode.......");
 			fo.addArguments("--incognito");
 		}
 		return fo;
-		
 	}
-
+	
+	
 	public EdgeOptions getEdgeOptions() {
 		eo = new EdgeOptions();
 		
 		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
 			eo.setCapability("enableVNC", true);
-			eo.setPlatformName("linux");	
+			eo.setPlatformName("linux");
 		}
 		
-		if(Boolean.parseBoolean(prop.getProperty("headless"))) {
-			System.out.println("......Running the tests in headless mode");
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println(".....Running the test in Headless mode.......");
 			eo.setHeadless(true);
 		}
-		if(Boolean.parseBoolean(prop.getProperty("incognito"))) {
-			System.out.println("......Running the tests in incognito mode");
+		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
+			System.out.println(".....Running the test in Incognito mode.......");
 			eo.addArguments("--incognito");
 		}
 		return eo;
-		
 	}
+
 }
